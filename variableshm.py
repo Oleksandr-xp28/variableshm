cbbf14d8c73a1aa3c975f9d083c6a7a594714729
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-22 01:36:10 +0200

    Task 1 done

--- variableshm.py
+++ variableshm.py
@@ -1,1 +1,10 @@
 
+if __name__ == "__main__":
+    try:
+        first_num = input("Enter the first digit: ")
+        second_num = input("Enter the second digit: ")
+        third_num = input("Enter the third digit: ")
+        number = int(first_num + second_num + third_num)
+        print(f'{number}')
+    except Exception as ex:
+        print(f'log: {ex}')
