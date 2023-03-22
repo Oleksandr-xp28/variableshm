2e7b7e681fbb7c12ea741cb99ba3d6c28ed2f941
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-22 14:48:03 +0200

    Ex_4 task done

--- variableshm.py
+++ variableshm.py
@@ -1,1 +1,9 @@
 
+if __name__ == "__main__":
+    try:
+        base = float(input("Enter the base of the triangle: "))
+        height = float(input("Enter the height of the triangle: "))
+        area = 0.5 * base * height
+        print(f"The area of the triangle is {area}")
+    except Exception as ex:
+        print(f'log: {ex}')
