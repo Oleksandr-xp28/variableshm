4e62eacfe3bfe795607f2d7de14d7b0e4cf9b877
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-22 14:43:34 +0200

    Ex_3 task done

--- variableshm.py
+++ variableshm.py
@@ -1,1 +1,18 @@
+if __name__ == "__main__":
+    try:
+        meters = float(input("Enter a distance in meters: "))
+
+        centimeters = meters * 100
+        decimeters = meters * 10
+        millimeters = meters * 1000
+        miles = meters / 1609.34
+
+        print(f"{meters} meters is equal to:")
+        print(f"{centimeters} centimeters")
+        print(f"{decimeters} decimeters")
+        print(f"{millimeters} millimeters")
+        print(f"{miles} miles")
+
+    except Exception as ex:
+        print(f'log: {ex}')
 
