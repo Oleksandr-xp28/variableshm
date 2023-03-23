5427a0fefb3849ac2bc1930dfab91aa29fd22b35
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-23 21:32:33 +0200

    Ex_5 task done

--- variableshm.py
+++ variableshm.py
@@ -1,1 +1,14 @@
+try:
+    number = int(input("Enter a four-digit number: "))
+    thousands = number // 1000
+    hundreds = (number // 100) % 10
+    tens = (number // 10) % 10
+    ones = number % 10
+
+    flipped_number = ones * 1000 + tens * 100 + hundreds * 10 + thousands
+
+    print(f"The flipped number is {flipped_number}")
+    pass
+except :
+    pass
 
